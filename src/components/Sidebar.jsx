5e430d74d3bd7d7513e98fc667020dba6c5da2f0
Nikola Vasiljevic <vasiljevicnikolabps@gmail.com>
2025-07-01 00:35:42 +0200

    Implements navigation link and and setting functions

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,69 +1,73 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import {
+  HiOutlineHashtag,
+  HiOutlineHome,
+  HiOutlineMenu,
+  HiOutlineUserGroup,
+  HiOutlineHeart,
+} from "react-icons/hi";
 import { RiCloseLine } from "react-icons/ri";
 
 import { logo } from "../assets";
-import { links } from "../assets/constants";
 
-const NavLinks = ({ handleClick }) => {
-  return (
-    <div className="mt-10">
-      {links.map((item) => (
-        <NavLink
-          key={item.name}
-          to={item.to}
-          className={({ isActive }) =>
-            isActive
-              ? "flex flex-row justify-start items-center my-8 text-sm font-medium text-cyan-400"
-              : "flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-cyan-400"
-          }
-          onClick={() => handleClick && handleClick()}
-        >
-          <item.icon className="w-6 h-6 mr-2" />
-          {item.name}
-        </NavLink>
-      ))}
-    </div>
-  );
-};
+// Navigation links
+const links = [
+  { name: "Discover", to: "/", icon: HiOutlineHome },
+  { name: "Top Artists", to: "/top-artists", icon: HiOutlineUserGroup },
+  { name: "Top Charts", to: "/top-charts", icon: HiOutlineHashtag },
+  { name: "Favorites", to: "/favorites", icon: HiOutlineHeart },
+];
 
-const Sidebar = ({ mobileMenuOpen, setMobileMenuOpen }) => {
+const NavLinks = ({ handleClick }) => (
+  <div className="mt-10">
+    {links.map((item) => (
+      <NavLink
+        key={item.name}
+        to={item.to}
+        className="flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-cyan-400"
+        onClick={() => handleClick && handleClick()}
+      >
+        <item.icon className="w-6 h-6 mr-2" />
+        {item.name}
+      </NavLink>
+    ))}
+  </div>
+);
+
+const Sidebar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
   return (
     <>
-      {/* Desktop Sidebar */}
       <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
-        <div className="flex justify-center items-center bg-[#191624]">
-          <img src={logo} alt="logo" className="w-full h-14 object-contain" />
-        </div>
+        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
         <NavLinks />
       </div>
 
-      {/* Mobilni hamburger meni */}
-      {mobileMenuOpen && (
-        <>
-          {/* Overlay za zatvaranje menija klikom izvan njega */}
-          <div
-            className="absolute top-0 left-0 w-full h-screen bg-black/50 backdrop-blur-sm md:hidden z-[5]"
+      {/* Mobile sidebar */}
+      <div className="absolute md:hidden block top-6 right-3">
+        {!mobileMenuOpen ? (
+          <HiOutlineMenu
+            className="w-6 h-6 mr-2"
+            onClick={() => setMobileMenuOpen(true)}
+          />
+        ) : (
+          <RiCloseLine
+            className="w-6 h-6 mr-2"
             onClick={() => setMobileMenuOpen(false)}
           />
+        )}
+      </div>
 
-          {/* Mobilni meni */}
-          <div className="absolute top-0 left-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483d8b] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition slide-in">
-            <div className="flex items-center justify-between">
-              <img
-                src={logo}
-                alt="logo"
-                className="w-[120px] h-14 object-contain"
-              />
-              <RiCloseLine
-                className="w-6 h-6 text-white cursor-pointer"
-                onClick={() => setMobileMenuOpen(false)}
-              />
-            </div>
-            <NavLinks handleClick={() => setMobileMenuOpen(false)} />
-          </div>
-        </>
-      )}
+      <div
+        className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483D8B] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${
+          mobileMenuOpen ? "left-0" : "-left-full"
+        }`}
+      >
+        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+        <NavLinks handleClick={() => setMobileMenuOpen(false)} />
+      </div>
     </>
   );
 };
