4da0ee1f59fa1eb964c0924114570e74b98d0b42
Nikola Vasiljevic <vasiljevicnikolabps@gmail.com>
2025-07-01 00:54:02 +0200

    Update src

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,34 @@
-import { useState } from "react";
 import { useSelector } from "react-redux";
 import { Route, Routes } from "react-router-dom";
-import { HiOutlineMenu } from "react-icons/hi";
 
 import { Searchbar, Sidebar, MusicPlayer, TopPlay } from "./components";
+
 import {
   ArtistDetails,
   TopArtists,
-  AroundYou,
   Discover,
   Search,
   SongDetails,
   TopCharts,
+  Favorites,
 } from "./pages";
-import "./index.css";
 
 const App = () => {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   const { activeSong, isActive } = useSelector((state) => state.player);
-
-  console.log("App rendering - isActive:", isActive, "activeSong:", activeSong);
 
   return (
     <div className="relative flex">
-      <Sidebar
-        mobileMenuOpen={mobileMenuOpen}
-        setMobileMenuOpen={setMobileMenuOpen}
-      />
-
+      <Sidebar />
       <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
         <Searchbar />
-
-        {/* Hamburger ikona za mobilne uređaje */}
-        <div className="absolute md:hidden top-6 right-3">
-          <HiOutlineMenu
-            className="w-6 h-6 text-white cursor-pointer"
-            onClick={() => setMobileMenuOpen(true)}
-          />
-        </div>
 
         <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
           <div className="flex-1 h-fit pb-40">
             <Routes>
               <Route path="/" element={<Discover />} />
-              <Route path="/top-artists" element={<TopArtists />} />
-              <Route path="/top-charts" element={<TopCharts />} />
-              <Route path="/around-you" element={<AroundYou />} />
+              <Route path="/top-artists" element={<TopArtists />} />{" "}
+              <Route path="/top-charts" element={<TopCharts />} />{" "}
+              <Route path="/favorites" element={<Favorites />} />
               <Route path="/artists/:id" element={<ArtistDetails />} />
               <Route path="/songs/:songid" element={<SongDetails />} />
               <Route path="/search/:searchTerm" element={<Search />} />
@@ -57,7 +40,7 @@ const App = () => {
         </div>
       </div>
 
-      {isActive && (
+      {activeSong?.attributes?.name && isActive && (
         <div className="absolute h-28 bottom-0 left-0 right-0 flex animate-slideup bg-gradient-to-br from-white/10 to-[#2a2a80] backdrop-blur-lg rounded-t-3xl z-10">
           <MusicPlayer />
         </div>
